0c831e3c50e9a1387be4d4e8cf4736d33a765a0a
Mohd Shariq <155702117+mohdsha-123@users.noreply.github.com>
2025-02-16 12:34:35 +0530

    handling multiple routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.use("/test",(req, res) => {
-  res.send(" namaste ! Hello from the server");
-});
+app.get('/user', (req, res, next) => {
+    console.log("Handling route 1!")
+    next();
+}, (req,res,next) => {
+  console.log("Handling route 2!");
+  next();
+},(req,res,next) => {
+  console.log("Handling route 3!");
+  next();
+},
+(req,res,next) => {
+  console.log("Handling route 4!");
+  next();
+}, (req,res,next) => {
+  console.log("Handling route 5!");
+  res.send("Hello, World!");
+})
 
 app.listen(3000, () => {
   console.log(`server is running on port 3000`);
